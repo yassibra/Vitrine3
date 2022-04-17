bd1e104f530aa722cf1eb7dcadbc21c55ea3e565
yassibra <tahriyassinepro@gmail.com>
2022-04-17 17:16:00 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# daisyui-svelte-vite-ofkjg9
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/daisyui-svelte-vite-ofkjg9)
---
+++ src/Home.svelte
@@ -0,0 +1,26 @@
+<script>
+  import { onMount } from 'svelte';
+  import Nav from '../src/Nav.svelte';
+  import Footer from '../src/Footer.svelte';
+  import Section from '../src/Section.svelte';
+  import NotFound from '../src/NotFound.svelte';
+  import Connexion from '../src/Connexion.svelte';
+  let photos = [];
+
+	onMount(async () => {
+		const res = await fetch(`http://78.199.207.79.localhost:8000/users/`);
+		photos = await res.json();
+    console.log(photos)
+	});
+   const pkg = {
+    titresite: 'Telegram Genius'
+  };
+</script>
+<Nav {...pkg} />
+<Section/>
+<Footer/>
+{#each photos as photo}
+	<p>{photo.test}</p>
+{/each}
+<p>WSH</p>
+
